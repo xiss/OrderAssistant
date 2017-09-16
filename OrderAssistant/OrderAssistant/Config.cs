c44c4562e7b4b45097d399e3c1ea76f6e71bc0e8
Andrew <xigrus@yandex.ru>
2017-09-16 12:02:30 +0300

    Переделал парсер

--- OrderAssistant/OrderAssistant/Config.cs
+++ OrderAssistant/OrderAssistant/Config.cs
@@ -8,50 +8,65 @@ namespace OrderAssistant
 {
     public  static class Config
     {
-        /// <summary>
-        /// Импорт. Отчет остатки и обороты. Полное имя файла.
-        /// </summary>
-        public static string ImportOrderStocksAndTrafficFileName = "D:\\Dropbox\\dev\\git\\OrderAssistant\\OrderAssistant\\OrderAssistant\\bin\\Остатки для БД(новая).xls";
-        /// <summary>
-        /// Импорт. Отчет остатки и обороты. Первая строка с которой начинаются данные.
-        /// </summary>
-        public static int ImportOrderStocksAndTrafficFirstRow = 7;
-        /// <summary>
-        /// Импорт. Отчет остатки и обороты. Колонка содержащая название запчасти.
-        /// </summary>
-        public static int ImportOrderStocksAndTrafficColName = 18;
-        /// <summary>
-        /// Импорт. Отчет остатки и обороты. Колонка содержащая каталожный номер запчасти.
-        /// </summary>
-        public static int ImportOrderStocksAndTrafficColCatNumber = 4; 
-        /// <summary>
-        /// Импорт. Отчет остатки и обороты. Колонка содержащая код 1С.
-        /// </summary>
-        public static int ImportOrderStocksAndTrafficCol1CId = 23;
-        /// <summary>
-        /// Импорт. Отчет остатки и обороты. Колонка содержащая бренд.
-        /// </summary>
-        public static int ImportOrderStocksAndTrafficColBrend = 30;
-        /// <summary>
-        /// Импорт. Отчет остатки и обороты. Колонка содержащая производителя.
-        /// </summary>
-        public static int ImportOrderStocksAndTrafficColManufacturer = 29;
-        /// <summary>
-        /// Импорт. Отчет остатки и обороты. Колонка содержащая количество.
-        /// </summary>
-        public static int ImportOrderStocksAndTrafficColCount = 21;
-        /// <summary>
-        /// Импорт. Отчет остатки и обороты. Колонка содержащая себестоимость.
-        /// </summary>
-        public static int ImportOrderStocksAndTrafficColCost = 22;
-        /// <summary>   
-        /// Импорт. Отчет остатки и обороты. Колонка содержащая дату остатка.
-        /// </summary>
-        public static int ImportOrderStocksAndTrafficColDate = 2;
-        /// <summary>
-        /// Импорт. Отчет остатки и обороты. Колонка содержащая название склада.
-        /// </summary>
-        public static int ImportOrderStocksAndTrafficColStock = 3;
+	    public static class Import
+	    {
+			public static class OrderStocksAndTraffic
+			{
+				/// <summary>
+				/// Импорт. Отчет остатки и обороты. Полное имя файла.
+				/// </summary>
+				public static string FileName = "D:\\Dropbox\\dev\\git\\OrderAssistant\\OrderAssistant\\OrderAssistant\\bin\\Остатки для БД(новая).xls";
 
+				/// <summary>
+				/// Импорт. Отчет остатки и обороты. Первая строка с которой начинаются данные.
+				/// </summary>
+				public static int FirstRow = 7;
+
+				/// <summary>
+				/// Импорт. Отчет остатки и обороты. Колонка содержащая название запчасти.
+				/// </summary>
+				public static int ColName = 18;
+
+				/// <summary>
+				/// Импорт. Отчет остатки и обороты. Колонка содержащая каталожный номер запчасти.
+				/// </summary>
+				public static int ColCatNumber = 4;
+
+				/// <summary>
+				/// Импорт. Отчет остатки и обороты. Колонка содержащая код 1С.
+				/// </summary>
+				public static int Col1CId = 23;
+
+				/// <summary>
+				/// Импорт. Отчет остатки и обороты. Колонка содержащая бренд.
+				/// </summary>
+				public static int ColBrend = 30;
+
+				/// <summary>
+				/// Импорт. Отчет остатки и обороты. Колонка содержащая производителя.
+				/// </summary>
+				public static int ColManufacturer = 29;
+
+				/// <summary>
+				/// Импорт. Отчет остатки и обороты. Колонка содержащая количество.
+				/// </summary>
+				public static int ColCount = 21;
+
+				/// <summary>
+				/// Импорт. Отчет остатки и обороты. Колонка содержащая себестоимость.
+				/// </summary>
+				public static int ColCost = 22;
+
+				/// <summary>   
+				/// Импорт. Отчет остатки и обороты. Колонка содержащая дату остатка.
+				/// </summary>
+				public static int ColDate = 2;
+
+				/// <summary>
+				/// Импорт. Отчет остатки и обороты. Колонка содержащая название склада.
+				/// </summary>
+				public static int ColStock = 3;
+			}
+	    }
     }
 }
--- OrderAssistant/OrderAssistant/Program.cs
+++ OrderAssistant/OrderAssistant/Program.cs
@@ -7,6 +7,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
+using static System.String;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace OrderAssistant
@@ -15,176 +16,106 @@ namespace OrderAssistant
 	{
 		static void Main(string[] args)
 		{
-			var curWb = new Excel.Application().Workbooks.Open(Config.ImportOrderStocksAndTrafficFileName);
-			var curSheet = curWb.Worksheets.Item[1];
-			var curRow = Config.ImportOrderStocksAndTrafficFirstRow;
+			var curWb = new Excel.Application().Workbooks.Open(Config.Import.OrderStocksAndTraffic.FileName);
+			dynamic curSheet = curWb.Worksheets.Item[1];
+			var curRow = Config.Import.OrderStocksAndTraffic.FirstRow;
 			var lastRow = curSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
 			var curStock = new stock();
 			var curDate = new DateTime();
 			using (var context = new orderAssistantEntities())
 			{
-				do
+				while (curRow <= lastRow) 
 				{
 					// Date
-					if (Check(curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColDate].Value, true, true))
+					if (curSheet.Cells[curRow, Config.Import.OrderStocksAndTraffic.ColDate].Value != null)
 					{
-						//TODO Добавить логирование если дата не создалась
-						curDate = DateTime.Parse(curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColDate].Value);
-						Console.WriteLine(curDate.ToString(CultureInfo.InvariantCulture));
-						curRow++;
-						continue;
+						try
+						{
+							curDate = DateTime.Parse(curSheet.Cells[curRow, Config.Import.OrderStocksAndTraffic.ColDate].Value);
+							Console.WriteLine(curDate.ToString(CultureInfo.InvariantCulture));
+							curRow++;
+							continue;
+						}
+						catch (FormatException e)
+						{
+							//TODO Добавить логирование если дата не создалась
+							Console.WriteLine("Дата не является датой");
+							Console.WriteLine(e);
+							curRow++;
+							continue;
+						}
 					}
 					// Stock
-					if (Check(curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColStock].Value, true, true))
+					if (curSheet.Cells[curRow, Config.Import.OrderStocksAndTraffic.ColStock].Value != null)
 					{
-						//TODO Добавить логирование если склад не нашелся
-						curStock = TakeStock(curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColStock].Value, context);
-						Console.WriteLine(curStock.name);
-						curRow++;
-						continue;
+						try
+						{
+							curStock = TakeStock(curSheet.Cells[curRow, Config.Import.OrderStocksAndTraffic.ColStock].Value, context);
+							Console.WriteLine(curStock.name);
+							curRow++;
+							continue;
+						}
+						catch (Exception e)
+						{
+							//TODO Добавить логирование если склад не нашелся
+							Console.WriteLine(e);
+							curRow++;
+							continue;
+						}
 					}
-					// CatNumber
+					
 					string catNumber;
-					if (Check(curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColCatNumber].Value, true, true))
-					{
-						catNumber = curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColCatNumber].Value;
-					}
-					else
-					{
-						Console.WriteLine(string.Join("Ошибка в строке " , curRow , " столбец " , Config.ImportOrderStocksAndTrafficColCatNumber));//TODO в лог
-						curRow++;
-						continue;
-					}
-					// Name
 					string name;
-					if (Check(curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColName].Value, true, true))
-					{
-						name = curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColName].Value;
-					}
-					else
-					{
-						Console.WriteLine(string.Join("Ошибка в строке ", curRow, " столбец ", Config.ImportOrderStocksAndTrafficColName));//TODO в лог
-						curRow++;
-						continue;
-					}
-					// Count
 					decimal count;
-					if (Check(curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColCount].Value, isDb: true,
-						isNotNegative: true))
-					{
-						//TODO может быть ошибка
-						count = (decimal)curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColCount].Value;
-					}
-					else
-					{
-						Console.WriteLine(string.Join("Ошибка в строке ", curRow, " столбец ", Config.ImportOrderStocksAndTrafficColCount));//TODO в лог
-						curRow++;
-						continue;
-					}
-					// 1C Code
 					string id1C;
-					if (Check(curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficCol1CId].Value, true, true))
-					{
-						id1C = curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficCol1CId].Value;
-					}
-					else
-					{
-						Console.WriteLine(string.Join("Ошибка в строке ", curRow, " столбец ", Config.ImportOrderStocksAndTrafficCol1CId));//TODO в лог
-						curRow++;
-						continue;
-					}
-					// Manufacturer
 					string manufacturerStr;
-					if (Check(curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColManufacturer].Value, true, true))
-					{
-						manufacturerStr = curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColManufacturer].Value;
-					}
-					else
-					{
-						Console.WriteLine(string.Join("Ошибка в строке ", curRow, " столбец ", Config.ImportOrderStocksAndTrafficColManufacturer));//TODO в лог
-						curRow++;
-						continue;
-					}
-					// Brend
 					string brendStr;
-					if (Check(curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColBrend].Value, true, true))
-					{
-						brendStr = curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColBrend].Value;
-					}
-					else
-					{
-						Console.WriteLine(string.Join("Ошибка в строке ", curRow, " столбец ", Config.ImportOrderStocksAndTrafficColBrend));//TODO в лог
-						curRow++;
-						continue;
-					}
-					// Cost
 					decimal cost;
-					if (Check(curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColCost].Value, isDb: true, isNotNegative: true))
+					try
 					{
-						//TODO может быть ошибка
-						cost = (decimal)curSheet.Cells[curRow, Config.ImportOrderStocksAndTrafficColCost].Value;
+						catNumber = curSheet.Cells[curRow, Config.Import.OrderStocksAndTraffic.ColCatNumber].Value;
+						name = curSheet.Cells[curRow, Config.Import.OrderStocksAndTraffic.ColName].Value;
+						count = (decimal)curSheet.Cells[curRow, Config.Import.OrderStocksAndTraffic.ColCount].Value;
+						id1C = curSheet.Cells[curRow, Config.Import.OrderStocksAndTraffic.Col1CId].Value;
+						manufacturerStr = curSheet.Cells[curRow, Config.Import.OrderStocksAndTraffic.ColManufacturer].Value;
+						brendStr = curSheet.Cells[curRow, Config.Import.OrderStocksAndTraffic.ColBrend].Value;
+						cost = (decimal)curSheet.Cells[curRow, Config.Import.OrderStocksAndTraffic.ColCost].Value;
 					}
-					else
+					catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException e)
 					{
-						Console.WriteLine(string.Join("Ошибка в строке ", curRow, " столбец ", Config.ImportOrderStocksAndTrafficColCost));//TODO в лог
+						Console.WriteLine(e);
+						Console.WriteLine(Join("Ошибка в строке", curRow) );
 						curRow++;
 						continue;
 					}
 
+					// Проверка на отрицательные остатки
+					if (cost <= 0 || count <= 0)
+					{
+						curRow++;
+						Console.WriteLine("Себестоимость или количество меньше или равно нулю.");
+						continue;
+					}
+
+					// Проверка на пустые строки
+					if (IsNullOrEmpty(catNumber) ||
+						IsNullOrEmpty(name) || 
+						IsNullOrEmpty(id1C) || 
+						IsNullOrEmpty(manufacturerStr) ||
+					    IsNullOrEmpty(brendStr))
+					{
+						curRow++;
+						Console.WriteLine("Строка содержит пустые параметры");
+						continue;
+					}
 					Console.WriteLine(id1C);
 					var item = GetItem(name, id1C, manufacturerStr, brendStr, catNumber, context);
 					item.balances.Add(GetBalance(curDate, curStock, cost, item, count, context));
 					curRow++;
-				} while (curRow <= lastRow);
+				} 
 			}
 		}
-
-		/// <summary>
-		/// Проверяет соответствует ли значение checkable необходимым требованиям, требования задаются в виде набора деректив ДА/НЕТ
-		/// </summary>
-		/// <param name="checkable">Проверяемое</param>
-		/// <param name="isString">Должно быть строкой</param>
-		/// <param name="isNotEmptyString">Должно быть не пустой строкой</param>
-		/// <param name="isDb">Должно быть double</param>
-		/// <param name="isNotNegative">Не должно быть отрицательным</param>
-		/// <param name="isDecimal">Должно быть Decimal</param>
-		/// <returns></returns>
-		public static bool Check(dynamic checkable, bool isString = false, bool isNotEmptyString = false, bool isDb = false,
-			bool isNotNegative = false, bool isDecimal = false)
-		{
-			// Если не строка возвращаем ложь
-			if (isString && !(checkable is string))
-			{
-				return false;
-			}
-
-			// Если пустая строка возвращаем ложь
-			if (isNotEmptyString && checkable is null)
-			{
-				return false;
-			}
-
-			// Если это не число
-			if (isDb && !(checkable is double))
-			{
-				return false;
-			}
-
-			// Если это не decimal
-			//TODO не работает
-			if (isDecimal && !(checkable is decimal))
-			{
-				return false;
-			}
-
-			// Если число меньше 0
-			if (isNotNegative && checkable < 0)
-			{
-				return false;
-			}
-			return true;
-		}
-
+		
 		/// <summary>
 		/// Возвращает ссылку на бренд либо созданый либо найденный, ищет без учета регистра.
 		/// </summary>
@@ -203,16 +134,10 @@ namespace OrderAssistant
 				{
 					name = nameStr
 				};
-				context.brends.Add(newBrend);
-				context.SaveChanges();
+				brend = context.brends.Add(newBrend);
 			}
-			else
-			{
-				return brend;
-			}
-			return (from b in context.brends
-					where b.name.ToLower().Contains(nameStr.ToLower())
-					select b).FirstOrDefault();
+			context.SaveChanges();
+			return brend;
 		}
 
 		/// <summary>
@@ -234,16 +159,10 @@ namespace OrderAssistant
 				{
 					name = nameStr
 				};
-				context.manufacturers.Add(newManufacturer);
-				context.SaveChanges();
+				manufacturer = context.manufacturers.Add(newManufacturer);
 			}
-			else
-			{
-				return manufacturer;
-			}
-			return (from m in context.manufacturers
-					where m.name.ToLower().Contains(nameStr.ToLower()) 
-					select m).FirstOrDefault();
+			context.SaveChanges();
+			return manufacturer;
 		}
 
 		/// <summary>
@@ -275,12 +194,7 @@ namespace OrderAssistant
 					name = name,
 					ABCgroup = "D" //TODO должно само в базе подставляться. но почемуто не хочет
 				};
-				context.items.Add(newItem);
-				context.SaveChanges();
-
-				item = (from i in context.items
-						where i.id1C == id1C
-						select i).FirstOrDefault();
+				item = context.items.Add(newItem);
 				return item;
 			}
 			// Иначе обновляем
@@ -320,7 +234,7 @@ namespace OrderAssistant
 					item = item,
 					count = count
 				};
-				context.balances.Add(newBalance);
+				balance = context.balances.Add(newBalance);
 			}
 			// Или обновляем
 			else
@@ -329,10 +243,7 @@ namespace OrderAssistant
 				balance.count = count;
 			}
 			context.SaveChanges();
-
-			return (from b in context.balances
-					where b.stock.id == stock.id && b.dateCount == date && b.item.id == item.id
-					select b).FirstOrDefault();
+			return balance;
 		}
 		/// <summary>
 		/// Возвращает Stock который подходит под определенную сигнатуру, без учета регистра
@@ -342,6 +253,10 @@ namespace OrderAssistant
 		/// <returns></returns>
 		public static stock TakeStock(string stockStr, orderAssistantEntities context)
 		{
+			if (IsNullOrEmpty(stockStr))
+			{
+				throw new Exception("Строка пуста или NULL");
+			}
 			return Enumerable.FirstOrDefault(context.stocks, stock => stockStr.ToLower().Contains(stock.signature));
 		}
 	}
